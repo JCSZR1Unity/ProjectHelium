e30444b2d6449efe0e5e8eec81f077fee4c95ed4
Marek <wilkmarek0@gmail.com>
2021-01-21 19:02:53 +0100

    fixed script so now cannons shoot properly, removed cannon's children's colliders and added one whole collider for it

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,56 +1,57 @@
 ﻿using UnityEngine;
 
-public class Bullet : MonoBehaviour
+namespace Assets.Scripts
 {
-    [SerializeField] float lifeTime = 5f;
-    public GameObject explosion;
-    private Rigidbody rigidBody;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Bullet : MonoBehaviour
     {
-        rigidBody = GetComponent<Rigidbody>();
-    }
+        [SerializeField] float lifeTime = 5f;
+        public GameObject explosion;
 
-    // Update is called once per frame
-    void Update()
-    {
-        LifeTimeCountdown();
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        switch (collision.gameObject.tag)
+        // Start is called before the first frame update
+        void Start()
         {
-            case "Player":
-                DestroyBullet();
-                //TODO: Implement kill player function or something
-                break;
-            case "Weapon":
-                Destroy(this.gameObject);
-                //TODO: on the player side - count score or something
-                //TODO: On Weapon script - fancy slice noise
-                break;
-            case "Cannon":
-                break;
-            default:
-                DestroyBullet();
-                break;
         }
-    }
 
-    void LifeTimeCountdown()
-    {
-        lifeTime -= Time.deltaTime;
-        if (lifeTime <= 0)
+        // Update is called once per frame
+        void Update()
         {
-            DestroyBullet();
+            LifeTimeCountdown();
         }
-    }
-    void DestroyBullet()
-    {
-        //Instantiate(explosion, transform.position, transform.rotation);
-        //TODO: Get explosion asset and KABOOM audioclip
-        Destroy(this.gameObject);
+
+        void OnCollisionEnter(Collision collision)
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Player":
+                    DestroyBullet();
+                    //TODO: Implement kill player function or something
+                    break;
+                case "Weapon":
+                    Destroy(this.gameObject);
+                    //TODO: on the player side - count score or something
+                    //TODO: On Weapon script - fancy slice noise
+                    break;
+                case "Cannon":
+                    break;
+                default:
+                    DestroyBullet();
+                    break;
+            }
+        }
+
+        void LifeTimeCountdown()
+        {
+            lifeTime -= Time.deltaTime;
+            if (lifeTime <= 0)
+            {
+                DestroyBullet();
+            }
+        }
+        void DestroyBullet()
+        {
+            //Instantiate(explosion, transform.position, transform.rotation);
+            //TODO: Get explosion asset and KABOOM audioclip
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Assets.Scripts
+{
+    public class CannonController : MonoBehaviour
+    {
+        private float _nextShootTimer = 3f;
+        Random random = new Random();
+        private const int MINIMUM_TIME_TO_SHOOT_IN_MILISECONDS = 100;
+        private const int MAXIMUM_TIME_TO_SHOOT_IN_MILISECONDS = 1000;
+        private const int DIVIDER_TO_CHANGE_MS_TO_SECONDS = 1000;
+
+        public GameObject newCannon;
+        public GameObject bullet;
+        public float firePower = 2000;
+        private Transform shotPosition;
+
+        public List<GameObject> cannons = new List<GameObject>();
+        // Start is called before the first frame update
+        void Start()
+        {
+            CreateCannons();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Shooting();
+        }
+
+        //Instantiates few gameobjects from cannon prefab
+        //If we build arena, we can create public list of coordinates where should cannons spawn
+        //And use it here instead of hardcoded values
+        void CreateCannons()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                GameObject cannon = Instantiate(newCannon, new Vector3((i + 1) * 10, 0, 0), this.gameObject.transform.rotation);
+                cannons.Add(cannon);
+            }
+        }
+
+        void Shooting()
+        {
+            _nextShootTimer -= Time.deltaTime;
+            if (_nextShootTimer < 0)
+            {
+                var cannonIndex = random.Next(0, 3);
+                var cannon = cannons[cannonIndex];
+                ReadyCannonballToShoot(cannon);
+            
+                _nextShootTimer = (float)random.Next(MINIMUM_TIME_TO_SHOOT_IN_MILISECONDS, MAXIMUM_TIME_TO_SHOOT_IN_MILISECONDS)/DIVIDER_TO_CHANGE_MS_TO_SECONDS;
+            }
+        }
+
+        void ReadyCannonballToShoot(GameObject cannon)
+        {
+            shotPosition = cannon.transform.Find("Cannon body").Find("Cannon Barrel").Find("ShotPosition").transform;
+            firePower = 2000;
+            GameObject cannonBallCopy = Instantiate(bullet,
+                shotPosition.position,
+                shotPosition.rotation);
+            var cannonBallRB = cannonBallCopy.GetComponent<Rigidbody>();
+            cannonBallRB.AddForce(-transform.forward * firePower);
+        }
+    }
+}
