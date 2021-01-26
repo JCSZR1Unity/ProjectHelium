7b5805020d75c9751c1323b6ac35e660ffcc46dc
Joanna Bukowska <joabuk2@gmail.com>
2021-01-26 22:40:26 +0100

    Added script to quit game and move around the menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,9 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame ()
+    public void PlayGame()
     {
         //loading next scene by pressing Play button
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        //quiting the program
+        Debug.Log("QUIT!");
+        Application.Quit();
     }
 }
