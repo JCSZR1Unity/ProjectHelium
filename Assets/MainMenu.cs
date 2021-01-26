9846e138080a927d5fba9f148efe31201ee4f2fc
Joanna Bukowska <joabuk2@gmail.com>
2021-01-26 22:34:21 +0100

    Added script for play button

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        //loading next scene by pressing Play button
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
